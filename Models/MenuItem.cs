c081b79eb9ffc9a182de50f6fde46d67737f9b6f
Naresh Kumar Rai <n.rai@student.fontys.nl>
2024-04-10 01:12:57 +0200

    Added Models

---
+++ Models/MenuItem.cs
@@ -0,0 +1,40 @@
+﻿namespace Restuarant_Order.Models
+{
+    public class MenuItem
+    {
+        public int MenuItemId { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+        public Hotness Hotness { get; set; } = Hotness.Zero;
+        public Type Type { get; set; }
+        public string? Others { get; set; }
+
+        public MenuItem (int id, string name, string? description, string? image, string? others, Hotness hotness, Type type)
+        {
+            MenuItemId = id;
+            Name = name;
+            Description = description;
+            Image = image;
+            Hotness = hotness;
+            Type = type;
+            Others = others;
+        }
+    }
+    public enum Type
+    {
+        Starter = 0,
+        Main = 1,
+        Side = 2,
+        Dessert = 3,
+        Drink = 4
+    }
+    public enum Hotness
+    {
+        Zero = 0,
+        Mild = 1,
+        Medium = 2,
+        Hot = 3,
+        ExtraHot = 4,
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,28 @@
+﻿namespace Restuarant_Order.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public required TimeOnly PlacedAt { get; set; }
+        public required DateOnly PlacedOn { get; set; }
+        public required int TableNumber { get; set; }
+        public string? Other { get; set; }
+        public OrderStatus OrderStatus { get; set; } = OrderStatus.Pending;
+
+        public Order (int orderId, TimeOnly placedAt, DateOnly placedOn, int tableNumber, string? other, OrderStatus orderStatus)
+        {
+            OrderId = orderId;
+            PlacedAt = placedAt;
+            PlacedOn = placedOn;
+            TableNumber = tableNumber;
+            Other = other;
+            OrderStatus = orderStatus;
+        }
+    }
+    public enum OrderStatus
+    {
+        Pending = 0,
+        Cooking = 1,
+        Served = 2,
+    }
+}
